5ec0cb7af433676d1471e530b029a73cb52467da
marcel <marcelmaier.htc@gmail.com>
2018-03-01 23:05:53 +0100

    fixed deprecated test + added librarys

--- src/application/tests/TestSlave.java
+++ src/application/tests/TestSlave.java
@@ -10,9 +10,7 @@ import org.junit.Test;
 import application.primes.PrimeFinder;
 import application.solver.PrimesRandomWalkSolver;
 import application.solver.XYCoordinateTupel;
-import junit.framework.Assert;
 
-@SuppressWarnings("deprecation")
 public class TestSlave {
 
 	private static XYCoordinateTupel xYCoordinateTupel;
@@ -27,38 +25,37 @@ public class TestSlave {
 	@Test
 	public void testXYTupel(){
 		assertNotNull(xYCoordinateTupel);
-		Assert.assertEquals(0, xYCoordinateTupel.getxCoordinate());
-		Assert.assertEquals(0, xYCoordinateTupel.getyCoordinate());
+		assertEquals(0, xYCoordinateTupel.getxCoordinate());
+		assertEquals(0, xYCoordinateTupel.getyCoordinate());
 		xYCoordinateTupel.setxCoordinate(5);
-		Assert.assertEquals(5, xYCoordinateTupel.getxCoordinate());
+		assertEquals(5, xYCoordinateTupel.getxCoordinate());
 		xYCoordinateTupel.setyCoordinate(11);
-		Assert.assertEquals(11, xYCoordinateTupel.getyCoordinate());
+		assertEquals(11, xYCoordinateTupel.getyCoordinate());
 	}
 	
 	@Test
 	public void testPrimesRandomWalkSolver(){
 		ArrayList<XYCoordinateTupel> arrayList = primesRandomWalkSolver.start();
 		assertNotNull(arrayList);
-		System.out.println(arrayList.get(arrayList.size()-1));
 		assertNotNull(primesRandomWalkSolver.getPrimes());
-		Assert.assertEquals(true, primesRandomWalkSolver.isWalkLeft());
-		Assert.assertEquals(19185, arrayList.size());
-		Assert.assertEquals(2, arrayList.get(1).getxCoordinate());
-		Assert.assertEquals(0, arrayList.get(1).getyCoordinate());
-		Assert.assertEquals(2, arrayList.get(2).getxCoordinate());
-		Assert.assertEquals(1, arrayList.get(2).getyCoordinate());
-		Assert.assertEquals(5, arrayList.get(10).getxCoordinate());
-		Assert.assertEquals(5, arrayList.get(10).getyCoordinate());	
+		assertEquals(true, primesRandomWalkSolver.isWalkLeft());
+		assertEquals(19185, arrayList.size());
+		assertEquals(2, arrayList.get(1).getxCoordinate());
+		assertEquals(0, arrayList.get(1).getyCoordinate());
+		assertEquals(2, arrayList.get(2).getxCoordinate());
+		assertEquals(1, arrayList.get(2).getyCoordinate());
+		assertEquals(5, arrayList.get(10).getxCoordinate());
+		assertEquals(5, arrayList.get(10).getyCoordinate());	
 	}
 	
 	@Test
 	public void testPrimeFinder(){
 		ArrayList<Integer> arrayList = PrimeFinder.findPrimes(10);
 		assertNotNull(arrayList);
-		Assert.assertEquals(4, arrayList.size());
-		Assert.assertEquals(2,(int) arrayList.get(0));
-		Assert.assertEquals(3,(int) arrayList.get(1));
-		Assert.assertEquals(5,(int) arrayList.get(2));
-		Assert.assertEquals(7,(int) arrayList.get(3));
+		assertEquals(4, arrayList.size());
+		assertEquals(2,(int) arrayList.get(0));
+		assertEquals(3,(int) arrayList.get(1));
+		assertEquals(5,(int) arrayList.get(2));
+		assertEquals(7,(int) arrayList.get(3));
 	}
 }
